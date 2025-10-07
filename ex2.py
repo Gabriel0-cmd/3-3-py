9f8391c38fa16921cb815e99da3fc7b2ead1bef2
Gabriel0-cmd <gabrielzardetto@colegiopaulodetarso.com.br>
2025-10-07 02:20:20 +0000

    opa

--- ex2.py
+++ ex2.py
@@ -1,4 +1,6 @@
 def soma(A, B):
     return A + B
 
+print(soma(1, 2))
+print(soma("a", "b"))
 print(soma("1", "2"))
---
+++ ex3.py
@@ -0,0 +1,8 @@
+valores = [
+    5,
+    4 + 2j,
+    "OBA",
+]
+
+for valor in valores: 
+    print(f"Valor: {valor} - Tipo: {type(valor)} - Nome: {type(valor)}")
